6fcea8e96f96e841d8759528bc6fb1e7bd688370
Jérôme <j.massip24@gmail.com>
2016-11-15 14:09:09 +0100

    Add Family & Model Entity

---
+++ src/CS/ShopBundle/Entity/Model.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace CS\ShopBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Model
+ *
+ * @ORM\Table(name="model")
+ * @ORM\Entity(repositoryClass="CS\ShopBundle\Repository\ModelRepository")
+ */
+class Model
+{
+    
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=100)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Model
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
