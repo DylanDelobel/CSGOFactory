32b87d36e7b69927c2f8c6c572f50c5e9f3300f5
Jérôme <j.massip24@gmail.com>
2016-12-12 18:55:34 +0100

    Update Checkout (Controller And View)

--- src/CS/ShopBundle/Controller/CheckoutController.php
+++ src/CS/ShopBundle/Controller/CheckoutController.php
@@ -3,16 +3,19 @@
 namespace CS\ShopBundle\Controller;
 
 
+use CS\ShopBundle\Entity\User;
+use CS\ShopBundle\Entity\UsersAdressesInfo;
 use CS\ShopBundle\ShopBundle;
 use Doctrine\ORM\Query\Expr\From;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+
 
 
 
@@ -20,22 +23,42 @@ class CheckoutController extends Controller
 {
 
     /**
+     * @Route("/checkout/address" , name="checkout_address")
+     */
+    public function addressAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+
+        $userId =$this->container->get('security.token_storage')->getToken()->getUser()->getId();
+
+
+        $infosUser = $em->getRepository('ShopBundle:UsersAdressesInfo')->findBy(array('user' => $userId));
+
+        return $this->render('ShopBundle:Checkout:selectAddress.html.twig', array('infosUser'=> $infosUser));
+    }
+
+    /**
      * @Route("/checkout/details" , name="checkout_details")
      */
-    public function detailsAction()
+    public function detailsAction(Request $request)
     {
 
+        $user = $this->container->get('security.token_storage')->getToken()->getUser();
+
+        $usersAdressesInfo = new UsersAdressesInfo();
+
         // On crée le FormBuilder
-        $formBuilder = $this->createFormBuilder();
+        $formBuilder = $this->createFormBuilder($usersAdressesInfo);
 
         // On ajoute les champs de l'entité que l'on veut à notre formulaire
         $formBuilder
-            ->add('email', EmailType::class, array(
+            ->add('emailPro', EmailType::class, array(
                 'required'    => true,
                 'empty_data'  => null
             ))
 
-            ->add('phone', IntegerType::class, array(
+            ->add('phone', TextType::class, array(
                 'required'    => true,
                 'empty_data'  => null
             ))
@@ -60,7 +83,7 @@ class CheckoutController extends Controller
                 'empty_data'  => null
             ))
 
-            ->add('postalCode', TextType::class, array(
+            ->add('cp', TextType::class, array(
                 'required'    => true,
                 'empty_data'  => null
             ))
@@ -71,13 +94,44 @@ class CheckoutController extends Controller
             ))
 
 
-            ->add('searchBtn', SubmitType::class, array('label' => 'Place My Order'));
+            ->add('save', SubmitType::class, array('label' => 'Place My Order'));
 
         // À partir du formBuilder, on génère le formulaire
         $form = $formBuilder->getForm();
 
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $usersAdressesInfo = $form->getData();
+
+            $usersAdressesInfo->setUser($user);
+
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($usersAdressesInfo);
+            $em->flush();
+
+
+            return $this->redirectToRoute('checkout_address');
+        }
+
         return $this->render('ShopBundle:Checkout:index.html.twig', array('form' => $form->createView()));
 
+    }
+
+    /**
+     * @Route("/checkout/details/delete/{id}", name="address_delete")
+     */
+    public function deleteAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $infoUserDelete = $em->getRepository('ShopBundle:UsersAdressesInfo')->find($id);
+
+        $em->remove($infoUserDelete);
+        $em->flush();
+
+        return $this->redirectToRoute('checkout_address');
     }
 
 
--- src/CS/ShopBundle/Entity/User.php
+++ src/CS/ShopBundle/Entity/User.php
@@ -23,4 +23,14 @@ class User extends BaseUser
         parent::__construct();
         // your own logic
     }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 }
