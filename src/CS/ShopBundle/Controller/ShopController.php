aeff4ed84ed7abb2e42c9874745d536f7fe515a4
Jérôme <j.massip24@gmail.com>
2016-12-14 15:42:07 +0100

    Add Order in DataBase

--- src/CS/ShopBundle/Controller/ShopController.php
+++ src/CS/ShopBundle/Controller/ShopController.php
@@ -11,7 +11,6 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\HttpFoundation\Request;
-
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
