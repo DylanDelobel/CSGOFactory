49a9ea8fc154f94efce98c58b690891fe7a6dfce
Dylan Delobel <dylan.delobel66@gmail.com>
2017-01-22 22:16:18 +0100

    Add form CrateType

---
+++ src/CS/ShopBundle/Form/CrateType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace CS\ShopBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CrateType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CS\ShopBundle\Entity\Crate'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'cs_shopbundle_crate';
+    }
+
+
+}
