3866ae4a2beaf747643b0d646aa13f3bdcf805c2
Dylan Delobel <dylan.delobel66@gmail.com>
2017-03-06 03:58:26 +0100

    Update LoadUserAdressesInfo

--- src/AppBundle/DataFixtures/ORM/LoadUsersAdressesInfo.php
+++ src/AppBundle/DataFixtures/ORM/LoadUsersAdressesInfo.php
@@ -2,13 +2,9 @@
 
 namespace AppBundle\DataFixtures\ORM;
 
-
 use AppBundle\Entity\UsersAdressesInfo;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-use AppBundle\Entity\User;
-
-
 
 class LoadUsersAdressesInfo implements FixtureInterface
 {
@@ -17,9 +13,6 @@ class LoadUsersAdressesInfo implements FixtureInterface
      */
     public function load(ObjectManager $manager)
     {
-
-
-
         $info = array(
             'firstName' => array(
                 'Jérôme',
@@ -27,11 +20,8 @@ class LoadUsersAdressesInfo implements FixtureInterface
                 'Lisa',
                 'Alexandre',
                 'Paul',
-                'Martin',
-                'Didier',
                 'Marie',
                 'Emma',
-                'Fabien'
             ),
             'lastName' => array(
                 'Massip',
@@ -39,8 +29,6 @@ class LoadUsersAdressesInfo implements FixtureInterface
                 'Delobel',
                 'Alvarez',
                 'Golotion',
-                'Hakoon',
-                'Conton',
                 'Optolo'
             ),
             'streetAddress' => array(
@@ -48,24 +36,20 @@ class LoadUsersAdressesInfo implements FixtureInterface
                 '2 place la marche',
                 '56 rue du jean-jean',
                 '60 place alfred fosta',
-                '7 rue du ivra',
                 '25 chemin du lotuse',
                 '42 rue martin'
             ),
             'city' => array(
                 'Perpignan',
                 'Avignon',
-                'Toulouse',
                 'New-York',
                 'Londre',
                 'Paris',
-                'Kourou'
             ),
             'cp' => array(
                 '66000',
                 '72568',
                 '85624',
-                '32505',
                 '30256',
                 '02356'
             ),
@@ -75,12 +59,11 @@ class LoadUsersAdressesInfo implements FixtureInterface
                 'EN'
             ),
             'phone' => array(
-                '0609835715',
-                '0409565716',
-                '0493556717',
-                '0656835718',
-                '0609835719',
-                '0605235720'
+                '0600000001',
+                '0600000002',
+                '0600000003',
+                '0600000004',
+                '0600000005',
             ),
             'emailPro' => array(
                 'Jerome@gmail.com',
@@ -88,7 +71,6 @@ class LoadUsersAdressesInfo implements FixtureInterface
                 'Emma@gmail.com',
                 'Lisa@gmail.com',
                 'Alexandre@gmail.com',
-                'Fabien@gmail.com',
                 'Dylan@gmail.com'
             )
         );
@@ -96,27 +78,21 @@ class LoadUsersAdressesInfo implements FixtureInterface
         $user = $manager->getRepository("ShopBundle:User");
         $listUser = $user->findAll();
 
-
         for ($i=0; $i < 20; $i++) {
             $userInfo = new UsersAdressesInfo();
 
             $userInfo->setUser($listUser[mt_rand(0,1)]);
-            $userInfo->setFirstName($info['firstName'][mt_rand(0,9)]);
-            $userInfo->setLastName($info['lastName'][mt_rand(0,7)]);
-            $userInfo->setStreetAddress($info['streetAddress'][mt_rand(0,6)]);
-            $userInfo->setCity($info['city'][mt_rand(0,6)]);
-            $userInfo->setCp($info['cp'][mt_rand(0,5)]);
+            $userInfo->setFirstName($info['firstName'][mt_rand(0,6)]);
+            $userInfo->setLastName($info['lastName'][mt_rand(0,5)]);
+            $userInfo->setStreetAddress($info['streetAddress'][mt_rand(0,5)]);
+            $userInfo->setCity($info['city'][mt_rand(0,4)]);
+            $userInfo->setCp($info['cp'][mt_rand(0,4)]);
             $userInfo->setCountry($info['country'][mt_rand(0,2)]);
-            $userInfo->setPhone($info['phone'][mt_rand(0,5)]);
-            $userInfo->setEmailPro($info['emailPro'][mt_rand(0,6)]);
-
+            $userInfo->setPhone($info['phone'][mt_rand(0,4)]);
+            $userInfo->setEmailPro($info['emailPro'][mt_rand(0,5)]);
 
             $manager->persist($userInfo);
         }
-
-
-
-
         // Record them in the database
         $manager->flush();
     }
