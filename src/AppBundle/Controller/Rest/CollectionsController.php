d2b50bab80cdf665e1e66e7580010aaf2e1f6236
Dylan D <dylan.delobel66@gmail.com>
2017-03-16 16:52:38 +0100

    Add REST Collection Controller

--- src/AppBundle/Controller/Rest/CollectionsController.php
+++ src/AppBundle/Controller/Rest/CollectionsController.php
@@ -2,32 +2,83 @@
 
 namespace AppBundle\Controller\Rest;
 
+use AppBundle\Entity\Collection;
+use AppBundle\Entity\Image;
+use AppBundle\Form\Type\CollectionType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class CollectionsController extends Controller
 {
-    public function getCollectionsAction()
-    {
+    public function getCollectionsAction(){
         $em = $this->getDoctrine()->getManager();
-        $crateRepository = $em->getRepository('AppBundle:Collection');
-        $crates = $crateRepository->findAll();
-        return $crates;
+        $collections = $em->getRepository('AppBundle:Collection')->findAll();
+        if (empty($collections)) {
+            return array("code" => "404", "message" => "Not Found");
+        }
+        return $collections;
     }
-
     public function getCollectionAction($id)
     {
         $em = $this->getDoctrine()->getManager();
-        $crateRepository = $em->getRepository('AppBundle:Collection');
-        $crate = $crateRepository->findOneById($id);
+        $collection = $em->getRepository('AppBundle:Collection')->find($id);
+        if (empty($collection)) {
+            return array("code" => "404", "message" => "Not Found");
+        }
+        return $collection;
+    }
 
-        if (empty($crate)) {
-            return new JsonResponse(['message' => 'Collection not found'], Response::HTTP_NOT_FOUND);
+    public function postCollectionAction(Request $request) {
+        $image = new Image();
+        $image->setPath('http://placehold.it/256x198');
+
+        $collection = new Collection();
+        $collection->setName($request->get('name'));
+
+        $form = $this->createForm(CollectionType::class, $collection);
+        $form->submit($request->request->all());
+
+        if($form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($collection);
+            $em->flush();
+            return $collection;
+        } else {
+            return array("code" => "400", "message" => "Form is Invalid");
+        }
+    }
+
+    public function putCollectionAction($id, Request $request){
+        $em = $this->getDoctrine()->getManager();
+        $collection = $em->getRepository('AppBundle:Collection')->find($id);
+
+        if (empty($collection)) {
+            return array("code" => "404", "message" => "Not Found");
         }
 
-        return $crate;
+        $form = $this->createForm(CollectionType::class, $collection);
+        $form->submit($request->request->all());
+
+        if ($form->isValid()){
+            $em->merge($collection);
+            $em->flush();
+            return array("code" => "200", "message" => "Ok");
+        }
+        return array("code" => "400", "message" => "Form is Invalid");
+    }
+
+    public function deleteCollectionAction($id){
+        $em = $this->getDoctrine()->getManager();
+        $collection = $em->getRepository('AppBundle:Collection')->find($id);
+
+        if (empty($collection)) {
+            return array("code" => "404", "message" => "Not Found");
+        }
+
+        $em->remove($collection);
+        $em->flush();
+        return array("code" => "200", "message" => "Ok");
     }
 }
 
---
+++ src/AppBundle/Form/Type/CollectionType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CollectionType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')->add('date')->add('image');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Collection'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_collection';
+    }
+
+
+}
